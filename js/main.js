7292346edbfbae194544c6af285c834a1b1b746a
reemmohamed8433@gmail.com <reemmohamed8433@gmail.com>
2025-06-09 17:14:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+  window.onload = function () {
+  window.scrollTo(0, 0);
+};
+ const navbar = document.querySelector(".navbar");
+   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  window.addEventListener("scroll", () => {
+    scrollToTopBtn.style.display = window.scrollY > 300 ? "flex" : "none";
+    navbar.classList.toggle("scrolled", window.scrollY > 50);
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+    AOS.init({
+    offset: 120,
+    duration: 1000,
+    easing: 'ease-in-out',
+  });
